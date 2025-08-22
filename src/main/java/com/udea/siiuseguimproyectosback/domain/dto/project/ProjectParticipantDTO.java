6d3683b9a769e13beb0da6dabee0fa41c05960ad
andres.tirado <andres.tirado@udea.edu.co>
2025-08-22 13:06:23 -0500

    Cambios en participante proyecto rol

--- src/main/java/com/udea/siiuseguimproyectosback/domain/dto/project/ProjectParticipantDTO.java
+++ src/main/java/com/udea/siiuseguimproyectosback/domain/dto/project/ProjectParticipantDTO.java
@@ -8,6 +8,14 @@ import java.util.List;
 public class ProjectParticipantDTO {
     private String id;
     private String project;
+    private String fullName;
     private String responsible;
     private List<ProjectParticipantRolDTO> projectParticipantRole;
+    private Long group;
+    private Integer dedicationMonths;
+    private Integer dedicationHours;
+    private Integer dedicationMonthsPlan;
+    private Integer dedicationHoursPlan;
+    private Long supportedProgramCode;
+    private Double academicProgPercentage;
 }
